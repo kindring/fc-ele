4dd4938ed8c09b359521098e05bb793c6b34e8cc
kindring <2460131004@qq.com>
2024-12-11 19:27:35 +0800

    暂存

--- src/apis/ApiAction.ts
+++ src/apis/ApiAction.ts
@@ -3,3 +3,9 @@ export enum Magnet_Actions {
   magnet_batch_update = 'magnet_batch_update',
   magnet_delete = 'magnet_delete',
 }
+
+
+export enum Music_Actions {
+  get_play_list= 'get_play_list',
+
+}
--- src/components/music/musicIndex.vue
+++ src/components/music/musicIndex.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import {defineComponent} from "vue";
+import {defineComponent, ref} from "vue";
+import IconSvg from "@/components/public/icon/iconSvg.vue";
+import {PlayList} from "@/types/musicType.ts";
 
 defineComponent({
   name: "musicIndex"
 })
+
+const searchText = ref('');
+
+const playList: PlayList = ref([
+  {
+    id: 0,
+
+  }
+])
+
 </script>
 
 <template>
@@ -15,8 +27,14 @@ defineComponent({
 
 <!--      侧边栏 -->
       <div class="search-input">
-
+        <input type="text" v-model="searchText" placeholder="搜索"/>
+        <div class="icon-search">
+          <IconSvg icon-name="search" />
+        </div>
       </div>
+
+<!--       主歌单 -->
+
     </div>
   </div>
 </template>
@@ -37,6 +55,51 @@ defineComponent({
   font-size: larger;
   display: block;
   padding-left: 0.8em;
+}
+
+.search-input{
+  width: calc( 100% - 20px);
+  height: 40px;
+  line-height: 40px;
+  margin: 0 10px;
+  border-radius: 0.5em;
+  background-color: var(--color-background-soft);
+  box-shadow: 1px 0 5px 0 var(--color-border);
+  display: flex;
+  position: relative;
+}
+.search-input input{
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border: none;
+  background-color: transparent;
+  padding-left: 10px;
+  padding-right: 50px;
+  flex-shrink: 0;
+  box-sizing: border-box;
+}
+
+.search-input .icon-search{
+  width: 50px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: var(--color-text);
+  flex-shrink: 0;
+  font-size: 1.5em;
+  position: absolute;
+  right: 0;
+}
+
+.search-input .icon-search:hover{
+  color: var(--color-text-show);
+  cursor: pointer;
+}
+.search-input .icon-search:active{
+  color: var(--color-text-money);
 }
 
 </style>
